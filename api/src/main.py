44d98c221bde8beba0b1c53bf42837b0653f10d6
Kinetics20 <kinetics20@gmail.com>
2025-09-10 23:19:59 +0200

    added endpoint health for FastAPI

--- api/src/main.py
+++ api/src/main.py
@@ -1,2 +1,12 @@
-def add(a, b):
-    return a + b
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title='Third Brain API',
+    version='0.0.1',
+    desription='Third Brain',
+)
+
+@app.get('/health')
+async def healthcheck():
+    return {'status': 'OK'}
