38eb790597b195e5fbbc0d74423001cf7d174a47
Kinetics20 <kinetics20@gmail.com>
2025-09-10 23:23:14 +0200

    added BaseSettings for OPENAI

---
+++ api/src/core/config.py
@@ -0,0 +1,8 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    OPENAI_API_KEY: str = Field(..., alias="OPENAI_API_KEY")
+
+
+settings = Settings()
